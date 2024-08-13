ad7628f4eff79f26d3d8529b22b768581cb993d3
SuichiNagi <aldreiglennkim@gmail.com>
2024-08-13 20:06:36 +0800

    Initial Commit

---
+++ Tip-Calculator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Tip-Calculator
+//
+//  Created by Aldrei Glenn Nuqui on 8/13/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

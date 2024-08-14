fff091ec63755fa98702711b5e001ead78630dd5
SuichiNagi <aldreiglennkim@gmail.com>
2024-08-14 09:27:48 +0800

    Add Logo View Details

--- Tip-Calculator/View/LogoView.swift
+++ Tip-Calculator/View/LogoView.swift
@@ -15,11 +15,56 @@ class LogoView: UIView {
     }
     
     private func setUI() {
-        backgroundColor = .red
+        addSubview(hStackView)
+        hStackView.snp.makeConstraints { make in
+            make.top.bottom.equalToSuperview()
+            make.centerX.equalToSuperview()
+        }
+        
+        imageView.snp.makeConstraints { make in
+            make.height.equalTo(imageView.snp.width)
+        }
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
+    
+    private lazy var topLabel: UILabel = {
+        let label = UILabel()
+        let text = NSMutableAttributedString(
+            string: "Mr TIP",
+            attributes: [.font: ThemeFont.demibold(ofSize: 16)])
+        text.addAttributes([.font: ThemeFont.bold(ofSize: 24)], range: NSMakeRange(3, 3))
+        label.attributedText = text
+        return label
+    }()
+    
+    private lazy var bottomLabel: UILabel = {
+        LabelFactory.build(text: "Calculator", font: ThemeFont.demibold(ofSize: 20), textAlignment: .left)
+    }()
+    
+    private lazy var imageView: UIImageView = {
+        let image = UIImageView(image: .icCalculatorBW)
+        image.contentMode = .scaleAspectFit
+        return image
+    }()
+    
+    private lazy var vStackView: UIStackView = {
+        let stackView = UIStackView(arrangedSubviews: [topLabel, bottomLabel])
+        stackView.axis = .vertical
+        stackView.spacing = -4
+        return stackView
+    }()
+    
+    private lazy var hStackView: UIStackView = {
+        let stackView = UIStackView(arrangedSubviews: [imageView, vStackView])
+        stackView.axis = .horizontal
+        stackView.spacing = 8
+        stackView.alignment = .center
+        return stackView
+    }()
+    
+    
     
 }
--- Tip-Calculator/factory/LabelFactory.swift
+++ Tip-Calculator/factory/LabelFactory.swift
@@ -5,4 +5,22 @@
 //  Created by Aldrei Glenn Nuqui on 8/14/24.
 //
 
-import Foundation
+import UIKit
+
+struct LabelFactory {
+    
+    static func build(
+        text: String?,
+        font: UIFont,
+        backgroundColor: UIColor = .clear,
+        textColor: UIColor = ThemeColor.text,
+        textAlignment: NSTextAlignment = .center) -> UILabel {
+            let label = UILabel()
+            label.text = text
+            label.font = font
+            label.backgroundColor = backgroundColor
+            label.textColor = textColor
+            label.textAlignment = textAlignment
+            return label
+        }
+}

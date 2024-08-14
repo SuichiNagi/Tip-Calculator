0d31bd406133f43416bb4fe6e29575e10fa855fd
SuichiNagi <aldreiglennkim@gmail.com>
2024-08-14 13:17:03 +0800

    Add Header View Details

--- Tip-Calculator/View/BillInputView.swift
+++ Tip-Calculator/View/BillInputView.swift
@@ -53,6 +53,7 @@ class BillInputView: UIView {
     
     private lazy var headerView: HeaderView = {
         let headerView = HeaderView()
+        headerView.configure(topText: "Enter", bottomText: "your bill")
         return headerView
     }()
     
@@ -109,7 +110,43 @@ class HeaderView: UIView {
         fatalError("init(coder:) has not been implemented")
     }
     
-    private func setUI() {
-        
+    func configure(topText: String, bottomText: String) {
+        topLabel.text = topText
+        bottomLabel.text = bottomText
     }
+    
+    private func setUI() {
+        addSubview(stackView)
+        stackView.snp.makeConstraints { make in
+            make.edges.equalToSuperview()
+        }
+        
+        topSpacerView.snp.makeConstraints { make in
+            make.height.equalTo(bottomSpacerView)
+        }
+    }
+    
+    private let topSpacerView = UIView()
+    private let bottomSpacerView = UIView()
+    
+    private lazy var stackView: UIStackView = {
+        let stackView = UIStackView(arrangedSubviews: [
+            topSpacerView,
+            topLabel,
+            bottomLabel,
+            bottomSpacerView
+        ])
+        stackView.axis = .vertical
+        stackView.alignment = .leading
+        stackView.spacing = -4
+        return stackView
+    }()
+    
+    private lazy var topLabel: UILabel = {
+        LabelFactory.build(text: nil, font: ThemeFont.bold(ofSize: 18))
+    }()
+    
+    private lazy var bottomLabel: UILabel = {
+        LabelFactory.build(text: nil, font: ThemeFont.regular(ofSize: 16))
+    }()
 }

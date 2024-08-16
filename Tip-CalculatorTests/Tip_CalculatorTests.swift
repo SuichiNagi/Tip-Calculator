e04b0451dcabe140f5746ca67c194d87bbafffd3
SuichiNagi <aldreiglennkim@gmail.com>
2024-08-16 20:45:20 +0800

    Add More Tests

--- Tip-CalculatorTests/Tip_CalculatorTests.swift
+++ Tip-CalculatorTests/Tip_CalculatorTests.swift
@@ -27,7 +27,7 @@ final class Tip_CalculatorTests: XCTestCase {
     override func tearDown() {
         super.tearDown()
         sut = nil
-        cancellable = .init()
+        cancellable = nil
     }
     
     func testResultWithoutTipFor1Person() {
@@ -46,6 +46,63 @@ final class Tip_CalculatorTests: XCTestCase {
             XCTAssertEqual(result.amountPerPerson, 100)
             XCTAssertEqual(result.totalBill, 100)
             XCTAssertEqual(result.totalTip, 0)
+        }.store(in: &cancellable)
+    }
+    
+    func testResultWithoutTipFor2Person() {
+        //given
+        let bill: Double = 100.0
+        let tip: Tip = .none
+        let split: Int = 2
+        let input = buildInput(
+            bill: bill,
+            tip: tip,
+            split: split)
+        //when
+        let output = sut.transform(input: input)
+        //then
+        output.updateViewPublisher.sink { result in
+            XCTAssertEqual(result.amountPerPerson, 50)
+            XCTAssertEqual(result.totalBill, 100)
+            XCTAssertEqual(result.totalTip, 0)
+        }.store(in: &cancellable)
+    }
+    
+    func testResultWith10PercentTipFor2Person() {
+        //given
+        let bill: Double = 100.0
+        let tip: Tip = .tenPercent
+        let split: Int = 2
+        let input = buildInput(
+            bill: bill,
+            tip: tip,
+            split: split)
+        //when
+        let output = sut.transform(input: input)
+        //then
+        output.updateViewPublisher.sink { result in
+            XCTAssertEqual(result.amountPerPerson, 55)
+            XCTAssertEqual(result.totalBill, 110)
+            XCTAssertEqual(result.totalTip, 10)
+        }.store(in: &cancellable)
+    }
+    
+    func testResultWithCustomPercentTipFor4Person() {
+        //given
+        let bill: Double = 200.0
+        let tip: Tip = .custom(value: 201)
+        let split: Int = 4
+        let input = buildInput(
+            bill: bill,
+            tip: tip,
+            split: split)
+        //when
+        let output = sut.transform(input: input)
+        //then
+        output.updateViewPublisher.sink { result in
+            XCTAssertEqual(result.amountPerPerson, 100.25)
+            XCTAssertEqual(result.totalBill, 401)
+            XCTAssertEqual(result.totalTip, 201)
         }.store(in: &cancellable)
     }
     
